ce8ffa4e814a64126bea4a19f42a017e76b3effd
Ian Lansdowne <idlansdowne@gmail.com>
2024-10-21 01:01:28 -0500

    This should make it boot again

--- pi/stage2/04-install-turtle-software/01-run.sh
+++ pi/stage2/04-install-turtle-software/01-run.sh
@@ -8,13 +8,11 @@ install -m 644 files/etc.dnsmasq.d.usb0 "${ROOTFS_DIR}/etc/dnsmasq.d/usb0"
 install -m 644 files/etc.network.interfaces.d.usb0 "${ROOTFS_DIR}/etc/network/interfaces.d/usb0"
 
 # Add dtoverlay to enable ethernet over USB
-tee -a "${ROOTFS_DIR}/boot/firmware/config.txt" << EOF 
+tee -a "${ROOTFS_DIR}/boot/firmware/config.txt" << EOF
 dtoverlay=dwc2,dr_mode=peripheral
 EOF
 
-tee -a "${ROOTFS_DIR}/boot/firmware/cmdline.txt" << EOF
-modules-load=dwc2,g_ether
-EOF
+echo "modules-load=dwc2,g_ether" >> "${ROOTFS_DIR}/boot/firmware/cmdline.txt"
 
 tee -a "${ROOTFS_DIR}/etc/dhcpcd.conf" << EOF
 denyinterfaces usb0
